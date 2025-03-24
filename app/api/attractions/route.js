e85a84233f91c57ebdf538b4d50cda26db00607c
Pacharadanai Pattarevarenon <big10555za@gmail.com>
2025-03-24 14:14:26 +0700

    add api create

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -1,11 +1,29 @@
 import { NextResponse } from "next/server";
 import { mysqlPool } from "@/utils/db";
 
+const db = mysqlPool.promise()
+
 export async function GET(request) {
-  const promisePool = mysqlPool.promise()
-  const [rows, fields] = await promisePool.query(
-    `SELECT * FROM attractions;`
-  )
-  return NextResponse.json(rows)
+  try {
+    const [rows, fields] = await db.query(
+      `SELECT * FROM attractions;`
+    )
+    return NextResponse.json(rows, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fatch" }, { status: 500 })
+  }
 }
+
+export async function POST(request){
+  try {
+      const {name, detail,coverimage, latitude, longitude} = await request.json();
+      const [result] = await db.query(
+          'INSERT INTO attractions (name, detail,coverimage, latitude, longitude) VALUES (?,?,?,?,?)',[name, detail,coverimage, latitude, longitude]
+      );
+      return NextResponse.json({id:result.insertId,name, detail,coverimage, latitude, longitude}, {status: 200});
+  } catch (error){
+      return NextResponse.json({error:error}, {status: 500})
+  }
+}
+
 
